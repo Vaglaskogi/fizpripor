3dbef747d2c14349ba00f09f5c323e7c8838b1f8
Konstantin <pribilkosta@gmail.com>
2022-04-14 16:35:16 +0300

    Готовый LandingPage

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let mapSwitch = document.querySelector('contacts-link')
+
+
+
+const swiper = new Swiper('.swiper', {
+  loop: true,
+  navigation: {
+    nextEl: '.slider-button__next',
+    prevEl: '.slider-button__prev',
+  },
+
+});
+
+mapSwitch.addEventListener("click", function(){
+  
+})
