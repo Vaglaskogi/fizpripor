940a8525f2f3a725b0d17f66415479e5addda820
Konstantin <pribilkosta@gmail.com>
2022-04-17 16:43:34 +0300

    Переделал слайдер

--- js/main.js
+++ js/main.js
@@ -1,16 +1,16 @@
-let mapSwitch = document.querySelector('contacts-link')
-
 
 
 const swiper = new Swiper('.swiper', {
+  autoplay:{
+    delay:3000,
+  },
   loop: true,
   navigation: {
     nextEl: '.slider-button__next',
     prevEl: '.slider-button__prev',
   },
+  pagination: {
+    el: '.swiper-pagination',
+  },
 
 });
-
-mapSwitch.addEventListener("click", function(){
-  
-})
